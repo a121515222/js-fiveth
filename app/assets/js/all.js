a74ce3f06cf7917b8895094a54542f9f2db3d457
CHUN-CHIA <a121515222@gmail.com>
2021-11-14 06:41:40 +0800

    add the alert counter and axios to get data

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,38 +1,4 @@
 
-let data = [
-  {
-    "id": 0,
-    "name": "肥宅心碎賞櫻3日",
-    "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-    "area": "高雄",
-    "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-    "group": 87,
-    "price": 1400,
-    "rate": 10
-  },
-  {
-    "id": 1,
-    "name": "貓空纜車雙程票",
-    "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    "area": "台北",
-    "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-    "group": 99,
-    "price": 2400,
-    "rate": 2
-  },
-  {
-    "id": 2,
-    "name": "台中谷關溫泉會1日",
-    "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    "area": "台中",
-    "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-    "group": 20,
-    "price": 1765,
-    "rate": 7
-  }
-];
-
-
 //DOM
 const search = document.querySelector('.search');
 const inputData = document.querySelectorAll('[data-input]');
@@ -41,7 +7,7 @@ const displayLocation = document.querySelector('.displayLocation');
 const showdataLen = document.querySelector('.dataLen');
 
 //全域變數
-
+let sideSeedata=[];
 let inputInfo = {
   ticketName: "can' be empty",
   ticketPictureUrl: "can' be empty",
@@ -54,12 +20,14 @@ let inputInfo = {
 let storageData = [];
 //功能
 function getUserInfo() {
+  let counter = 0;
   inputData.forEach(function (item, index) {
     switch (index) {
       case 0:
         if (item.value === '') {
-          alert(`套票名稱不可空白`)
-          inputData[index].focus()
+          alert(`套票名稱不可空白`);
+          inputData[index].focus();
+          counter++;
           return
         }
         inputInfo.ticketName = item.value;
@@ -72,8 +40,9 @@ function getUserInfo() {
         break
       case 3:
         if (item.value === '') {
-          alert(`套票價格不可空白`)
-          inputData[index].focus()
+          alert(`套票價格不可空白`);
+          inputData[index].focus();
+          counter++;
           return
         }
         inputInfo.ticketPrice = parseInt(item.value);
@@ -81,23 +50,26 @@ function getUserInfo() {
       case 4:
         if (item.value === '') {
           alert(`套票組不可空白`)
-          inputData[index].focus()
+          inputData[index].focus();
+          counter++;
           return
         }
         inputInfo.ticketSetNum = parseInt(item.value);
         break
       case 5:
         if (item.value === '') {
-          alert(`套票星級不可空白`)
-          inputData[index].focus()
+          alert(`套票星級不可空白`);
+          inputData[index].focus();
+          counter++;
           return
         }
         inputInfo.ticketRank = parseInt(item.value);
         break
       case 6:
         if (item.value === '') {
-          alert(`套票描述不可空白`)
-          inputData[index].focus()
+          alert(`套票描述不可空白`);
+          inputData[index].focus();
+          counter++;
           return
         }
         inputInfo.ticketDiscript = item.value;
@@ -105,12 +77,16 @@ function getUserInfo() {
     }
 
   });
-  render()
+  if (counter===0){reture}
+  else{
+    render()
+  }
+  
 }
 //compare Data
 function compareData() {
 
-  data.forEach(function (item) {
+  sideSeedata.forEach(function (item) {
     if (item.name.indexOf(inputInfo.ticketName) !== -1) {
       storageData.push(item);
     }
@@ -133,7 +109,7 @@ function compareData() {
       storageData.push(item);
     }
   })
-  console.log(storageData);
+  //console.log(storageData);
 }
 function showDataLen() {
   showdataLen.textContent = `本次搜尋共${storageData.length}筆資料`
@@ -188,6 +164,20 @@ function displaySelectionLocation(e) {
   inputInfo.ticketLocation = e.target.value;
   render()
 }
+//撈資料
+function getdata(){
+  axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json')
+  .then(function (response) {
+    sideSeedata=response.data.data;
+    getUserInfo();
+    //data=response.data[0].data;
+
+  })
+  .catch(function (error) {
+    console.log(error);
+  });  
+}
+
 
 function render() {
   compareData();
@@ -196,7 +186,8 @@ function render() {
   resetInputInfo();
 }
 function work() {
-  getUserInfo();
+  getdata();
+  
 }
 
 //監聽
