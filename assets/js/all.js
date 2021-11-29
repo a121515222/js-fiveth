bcc29a1574eaa80f4d39b0d1f0a7ac9481971202
CHUN-CHIA <a121515222@gmail.com>
2021-11-29 16:24:39 +0800

    Update 2021-11-29T08:24:36.975Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -6,13 +6,16 @@ var inputData = document.querySelectorAll('[data-input]');
 var selectLocation = document.querySelector('.selectLocation');
 var displayLocation = document.querySelector('.displayLocation');
 var showdataLen = document.querySelector('.dataLen');
-var inputfrom = document.querySelector('.inputfrom'); //全域變數
+var inputfrom = document.querySelector('.inputfrom');
+var errerMessage = document.querySelectorAll('form p'); //全域變數
 
 var sideSeedata = [];
-var inputinfoData = []; //功能
+var inputinfoData = [];
+console.log(inputData[6].value); //功能
 //取得資料後push進入inputinfoData
 
-function getUserInfo() {
+function getUserInfo(e) {
+  e.preventDefault();
   var data = {
     area: "",
     description: "",
@@ -23,100 +26,49 @@ function getUserInfo() {
     price: 0,
     rate: 0
   };
-  var counter = 0;
   inputData.forEach(function (item, index) {
     switch (index) {
       case 0:
-        if (item.value === '') {
-          alert("\u5957\u7968\u540D\u7A31\u4E0D\u53EF\u7A7A\u767D");
-          inputData[index].focus();
-          counter++;
-          return;
-        } else {
-          data.name = item.value;
-        }
-
+        data.name = item.value;
         break;
 
       case 1:
-        if (item.value === '') {
-          alert("\u5957\u7968\u5716\u7247\u4E0D\u53EF\u7A7A\u767D");
-          inputData[index].focus();
-          counter++;
-          return;
-        } else {
-          data.imgUrl = item.value;
-        }
-
+        data.imgUrl = item.value;
         break;
 
       case 2:
-        if (item.value === '') {
-          alert("\u5957\u7968\u5730\u5340\u4E0D\u53EF\u7A7A\u767D");
-          inputData[index].focus();
-          counter++;
-          return;
-        } else {
-          data.area = item.value;
-        }
-
+        data.area = item.value;
         break;
 
       case 3:
-        if (item.value === '') {
-          alert("\u5957\u7968\u50F9\u683C\u4E0D\u53EF\u7A7A\u767D");
-          inputData[index].focus();
-          counter++;
-          return;
-        } else {
-          data.price = Number(item.value);
-        }
-
+        data.price = Number(item.value);
         break;
 
       case 4:
-        if (item.value === '') {
-          alert("\u5957\u7968\u7D44\u4E0D\u53EF\u7A7A\u767D");
-          inputData[index].focus();
-          counter++;
-          return;
-        } else {
-          data.group = Number(item.value);
-        }
-
+        data.group = Number(item.value);
         break;
 
       case 5:
-        if (item.value === '') {
-          alert("\u5957\u7968\u661F\u7D1A\u4E0D\u53EF\u7A7A\u767D");
-          inputData[index].focus();
-          counter++;
-          return;
-        } else {
-          data.rate = item.value;
-        }
-
+        data.rate = item.value;
         break;
 
       case 6:
-        if (item.value === '') {
-          alert("\u5957\u7968\u63CF\u8FF0\u4E0D\u53EF\u7A7A\u767D");
-          inputData[index].focus();
-          counter++;
-          return;
-        } else {
-          data.description = item.value;
-        }
-
+        data.description = item.value;
         break;
     }
-  });
+  }); //顯示錯誤
 
-  if (counter === 0) {
+  errorDisplay(); //如果有錯誤訊息則不渲染畫面
+
+  if (errorDisplay()) {
+    console.log('error return');
+    return;
+  } else {
     inputinfoData.push(data);
     inputinfoData.forEach(function (item) {
       sideSeedata.push(item);
-    });
+    }); //渲染畫面
+
     render(sideSeedata); //重製表單
 
     inputfrom.reset();
@@ -158,7 +110,6 @@ function getdata() {
 }
 
 function render(data) {
-  // compareData();
   showDataNum(data);
   showData(data);
   pieChart();
@@ -202,6 +153,98 @@ function pieChart() {
       title: "套票地區比重"
     }
   });
+} //套件
+//VALIDATE.JS
+//限制條件
+
+
+function errorDisplay() {
+  var constraints = {
+    ticketName: {
+      presence: {
+        message: "必填欄位"
+      }
+    },
+    ticketPictureUrl: {
+      presence: {
+        message: "必填欄位"
+      },
+      url: {
+        schemes: ["http", "https"],
+        message: "必須是正確的網址"
+      }
+    },
+    location: {
+      presence: {
+        message: "必填欄位"
+      }
+    },
+    ticketPrice: {
+      presence: {
+        message: "必填欄位"
+      },
+      numericality: {
+        onlyInteger: true,
+        greaterThan: 0,
+        notInteger: "請填入整數",
+        notGreaterThan: "請填入大於0的整數"
+      }
+    },
+    ticketNum: {
+      presence: {
+        message: "必填欄位"
+      },
+      numericality: {
+        onlyInteger: true,
+        greaterThan: 0,
+        notInteger: "請填入整數",
+        notGreaterThan: "請填入大於0的整數"
+      }
+    },
+    ticketRank: {
+      presence: {
+        message: "必填欄位"
+      },
+      numericality: {
+        onlyInteger: true,
+        greaterThan: 0,
+        lessThanOrEqualTo: 5,
+        notInteger: "請填入整數",
+        notGreaterThan: "請填入大於0的整數",
+        notLessThanOrEqualTo: "請填入小於等於5的整數"
+      }
+    },
+    ticketDisctipt: {
+      presence: {
+        message: "必填欄位"
+      },
+      length: {
+        maximum: 100,
+        tooLong: "不可超過100字"
+      }
+    }
+  };
+  var errors = validate(inputfrom, constraints);
+  console.log(errors); //把錯誤訊息顯示出來有錯誤時是顯示空值
+
+  errerMessage.forEach(function (item, index) {
+    var classString = item.getAttribute('class'); //console.log(classString)
+
+    var classStringArray = classString.split(' ');
+
+    if (errors) {
+      //console.log(classStringArray[0])
+      if (Object.keys(errors).indexOf(classStringArray[0]) !== -1) {
+        errerMessage[index].textContent = errors[classStringArray[0]];
+      }
+
+      ;
+    } else {
+      errerMessage[index].textContent = '';
+    }
+  }); //回傳errors作為要不要reset的判斷依據
+
+  return errors; //console.log(errerMessage);
 } //直接執行
 
 
